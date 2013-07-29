5dfe8c4084fc6bf74b29ebcf6fc1c20f9b62af69
root <VAIO>
2013-07-29 20:16:42 +0530

    update Weather_Forecast

--- Weather_Forecast.py
+++ Weather_Forecast.py
@@ -1,10 +1,25 @@
 import urllib2
 import re
 
-source=urllib2.urlopen("http://www.accuweather.com/en/in/calcutta/206690/weather-forecast/206690")
+city=raw_input("Enter City:")
+
+if(city=="Kolkata" or city=="kolkata" or city=="KOLKATA"):
+    source=urllib2.urlopen("http://www.accuweather.com/en/in/calcutta/206690/weather-forecast/206690")
+elif(city=="Bangalore" or city=="bangalore"):
+    source=urllib2.urlopen("http://www.accuweather.com/en/in/bangalore/204108/weather-forecast/204108")
+elif(city=="Delhi"):
+    source=urllib2.urlopen("http://www.accuweather.com/en/in/delhi/202396/weather-forecast/202396")
+elif(city=="Chennai"):
+    source=urllib2.urlopen("http://www.accuweather.com/en/in/chennai/206671/weather-forecast/206671")
+
+else:
+    print "City not in Database.Choose a different one."
+
+
+
 
 text=source.read()
-print("<------------------------WEATHER REPORT: KOLKATA--------------------------->")
+print("<------------------------WEATHER REPORT: "+city.upper()+"--------------------------->")
 regex='<strong class="temp">(.+?)<span>(.+?)</span></strong>'
 regex1='<span class="cond">(.+?)</span>'
 pattern=re.compile(regex)
